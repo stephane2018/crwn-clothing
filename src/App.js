6c6f7c3a5232858b7384a0b4bde64b0bc3a072bf
stephane2018 <bitebstephane1996@gmail.com>
2019-12-11 11:42:07 +0000

    updating app shop page , collection previe, and collection-item component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Homepage from  './pages/homepage/homepage.component'
+import { Switch, Route, Link } from "react-router-dom";
+//import Homepage from  './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+
+const Homepage=props=>{
+  console.log(props);
+  return(
+  <div>
+    <Link to='/topics'>Topics</Link>
+    <h1>
+    Home page 
+    </h1>
+  </div>
+)}
+const HatsPage=()=>(
+  <div>
+    <h1>
+    HatsPage
+    </h1>
+  </div>
+)
+const TopicsList=()=>(
+  <div>
+    <h1>
+    HatsPage
+    </h1>
+  </div>
+)
+const TopicsDetails=(props)=>{
+  console.log(props)
+  return(
+    <div>
+   <h1> KList page </h1>
+  </div>
+  )
+}
+
 function App() {
   return (
     <div >
-      <Homepage/>
+    <Switch>  
+     <Route exact path='/' component={Homepage}/>
+      <Route exact path='/shop' component={ShopPage}/>
+      <Route path='/topics/:topicsId' component={TopicsDetails}/>
+    </Switch>
+   
     </div>
   );
 }
---
+++ src/components/preview-collection/preview-collection.componenet.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './preview-collection.style.scss'; 
+const previewCollection=()={
+ <div>
+ </div>
+}
+

2b686828e7370016a76708857aeea68cb7a31e4e
stephane2018 <bitebstephane1996@gmail.com>
2019-12-11 11:59:34 +0000

    adding header component to the projet

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import './App.css';
 import { Switch, Route, Link } from "react-router-dom";
-//import Homepage from  './pages/homepage/homepage.component';
+import Homepage from  './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-
-const Homepage=props=>{
-  console.log(props);
-  return(
-  <div>
-    <Link to='/topics'>Topics</Link>
-    <h1>
-    Home page 
-    </h1>
-  </div>
-)}
+import Header from './components/header/header.component'
+// const Homepage=props=>{
+//   console.log(props);
+//   return(
+//   <div>
+//     <Link to='/topics'>Topics</Link>
+//     <h1>
+//     Home page 
+//     </h1>
+//   </div>
+// )}
 const HatsPage=()=>(
   <div>
     <h1>
@@ -40,6 +40,7 @@ const TopicsDetails=(props)=>{
 function App() {
   return (
     <div >
+    <Header/>
     <Switch>  
      <Route exact path='/' component={Homepage}/>
       <Route exact path='/shop' component={ShopPage}/>

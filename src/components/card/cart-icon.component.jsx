a87634f4f9877bc787991381065147d342c35e5b
stephane2018 <bitebstephane1996@gmail.com>
2019-12-18 11:56:48 +0000

    updating our applcation with reselecct, replaced cart selectors with momorized

--- src/components/card/cart-icon.component.jsx
+++ src/components/card/cart-icon.component.jsx
@@ -3,16 +3,20 @@ import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg'
 import {toogleCartHidden} from "../../redux/cart/cart.actions"
 import {connect} from 'react-redux'
 import './cart-styles.scss'
-const CartIcon=({toogleCartHidden})=>(
+import CartItem from '../card-item/cart-item.component';
+import {selectCartItemsCount} from '../../redux/cart/cart.selectors'
+const CartIcon=({toogleCartHidden, itemsCount})=>(
     <div className='cart-icon' onClick={toogleCartHidden}>
         <ShoppingIcon className='shopping-icon'/>
-        <span className='item-count'>0</span>
+        <span className='item-count'>{itemsCount}</span> 
     </div>
 ); 
 
 const mapDispatchToProps =(dispatch) =>({
     toogleCartHidden: ()=> dispatch(toogleCartHidden())
-  
 })
+const mapStateToProps = (state) => ({
+  itemsCount: selectCartItemsCount(state)
+});
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
-
+import {connect} from 'react-redux'
 import  CustomButton from '../custom-button/custom-button.component'
 import './cart-dropdown.component.scss'
-
-const CartDropdown =()=>(
+import CartItem from '../card-item/cart-item.component'
+const CartDropdown =({cartItems})=>(
     <div className='cart-dropdown'>
         <div className='cart-items'/>
+        {
+            cartItems.map(cartItem => (
+                <CartItem key={cartItem.id} item={cartItem}/>
+            )) 
+        }
         <CustomButton> Go To CHECKOUT</CustomButton>
     </div>
 )
-export default CartDropdown; 
+const mapStateToprops =({cart: {cartItems}})=>({
+    cartItems 
+});
+export default connect( mapStateToprops)(CartDropdown); 
